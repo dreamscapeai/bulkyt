4345fdd6986f6c61540de827bd3f834e67e8d854
Dreamscape Youtube <142077967+dreamscapeai@users.noreply.github.com>
2024-05-17 18:41:03 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "youtube-bulk-downloader",
+  "version": "1.0.0",
+  "description": "YouTube Bulk Downloader",
+  "dependencies": {
+    "express": "^4.17.1",
+    "ytdl-core": "^4.9.1"
+  },
+  "scripts": {
+    "start": "node server.js"
+  },
+  "keywords": ["youtube", "bulk", "downloader"],
+  "author": "Your Name",
+  "license": "MIT"
+}
